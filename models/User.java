90ec2ae8dd9d2ef3032b063b3507fbf759cc1303
Hugo Maitre <hugo.maitre69@gmail.com>
2018-03-09 14:32:42 +0100

    Mise à jour User

--- models/User.java
+++ models/User.java
@@ -4,5 +4,40 @@ public class User {
 
 	private String pseudo; 
 	private String password;
+	private USERTYPE type;
+	
+	public enum USERTYPE {
+		FAMILY,
+		OWNER
+	}
+	
+	public User(String pseudo, String password, USERTYPE type) {
+		this.pseudo = pseudo;
+		this.password = password;
+	}
+	
+	public String getPseudo() {
+		return this.pseudo;
+	}
+	
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public USERTYPE getType() {
+		return this.type;
+	}
+	
+	public void setType(USERTYPE type) {
+		this.type = type;
+	}
 	
 }

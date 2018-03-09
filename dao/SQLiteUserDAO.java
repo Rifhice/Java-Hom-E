fceb2ceb04cd12e2906bcec96e5486d52ba99b41
Clément Roig <clm.roig@gmail.com>
2018-03-09 14:56:03 +0100

    BD : ça avance

---
+++ dao/SQLiteUserDAO.java
@@ -0,0 +1,31 @@
+package dao;
+
+import models.User;
+
+public class SQLiteUserDAO extends DAO<User> {
+
+	@Override
+	public boolean create(User obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public User getById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean update(User obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(User obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
--- factories/SQLiteDAOFactory.java
+++ factories/SQLiteDAOFactory.java
@@ -2,10 +2,15 @@ package factories;
 
 import java.sql.Connection;
 import dao.ConnectionDriver;
+import dao.DAO;
+import dao.SQLiteUserDAO;
 
-public class MySQLiteDAOFactory extends AbstractDAOFactory{
+public class SQLiteDAOFactory extends AbstractDAOFactory{
 	
 	protected static final Connection conn = ConnectionDriver.getInstance(); 
 	
+	public DAO getUserDAO() {
+		return new SQLiteUserDAO();
+	}
 	
 }

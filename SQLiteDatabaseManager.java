daeb5e96a7a0606ec907bcb68a08178f784d16fa
Hugo Maitre <hugo.maitre69@gmail.com>
2018-03-10 14:07:05 +0100

    BD

---
+++ SQLiteDatabaseManager.java
@@ -0,0 +1,35 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import dao.DriverConnection;
+
+public class SQLiteDatabaseManager {
+	
+	private static Connection conn;
+	
+	
+	public static void main(String args[]) {
+		conn = DriverConnection.getInstance();
+		SQLiteDatabaseManager.fillDatabase();
+	}
+	
+	private static void fillDatabase() {
+		String sql1 = "CREATE TABLE IF NOT EXISTS users (\n"
+                + "	id integer PRIMARY KEY,\n"
+                + "	pseudo text NOT NULL,\n"
+                + "	password text NOT NULL\n"
+                + ");";
+        String sql2 = "INSERT INTO users ('pseudo', 'password') VALUES ('owner', 'password');";
+        
+        try (Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql1);
+            stmt.execute(sql2);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        
+        		
+	}
+}
--- factories/SQLiteDAOFactory.java
+++ factories/SQLiteDAOFactory.java
@@ -1,13 +1,14 @@
 package factories;
 
 import java.sql.Connection;
-import dao.ConnectionDriver;
+
 import dao.DAO;
+import dao.DriverConnection;
 import dao.SQLiteUserDAO;
 
 public class SQLiteDAOFactory extends AbstractDAOFactory{
 	
-	protected static final Connection conn = ConnectionDriver.getInstance(); 
+	protected static final Connection conn = DriverConnection.getInstance(); 
 	
 	public DAO getUserDAO() {
 		return new SQLiteUserDAO();

b47bc8a6afb4386203d891515c16c07f95155388
Kevin giordani <giordani.kevin.02@gmail.com>
2018-03-10 14:31:41 +0100

    Correction push foireux

--- factories/SQLiteDAOFactory.java
+++ factories/SQLiteDAOFactory.java
@@ -1,22 +1,14 @@
 package factories;
 
 import java.sql.Connection;
-<<<<<<< HEAD
-import dao.SQLiteDriverConnection;
-=======
 
->>>>>>> branch 'develop' of https://github.com/Rifhice/Hom-E.git
 import dao.DAO;
 import dao.DriverConnection;
 import dao.SQLiteUserDAO;
 
 public class SQLiteDAOFactory extends AbstractDAOFactory{
 	
-<<<<<<< HEAD
-	protected static final Connection conn = SQLiteDriverConnection.getInstance(); 
-=======
 	protected static final Connection conn = DriverConnection.getInstance(); 
->>>>>>> branch 'develop' of https://github.com/Rifhice/Hom-E.git
 	
 	public DAO getUserDAO() {
 		return new SQLiteUserDAO();

c4f93a04355285be1d1ce88c88e51a9c5d7d1fa4
Clément Roig <clm.roig@gmail.com>
2018-03-12 23:05:10 +0100

    [DAO Factory] Remove useless Connection

--- factories/SQLiteDAOFactory.java
+++ factories/SQLiteDAOFactory.java
@@ -1,14 +1,9 @@
 package factories;
 
-import java.sql.Connection;
-
-import dao.DriverConnection;
 import dao.SQLiteUserDAO;
 import dao.UserDAO;
 
 public class SQLiteDAOFactory extends AbstractDAOFactory{
-	
-	protected static final Connection conn = DriverConnection.getInstance(); 
 	
 	public UserDAO getUserDAO() {
 		return new SQLiteUserDAO();

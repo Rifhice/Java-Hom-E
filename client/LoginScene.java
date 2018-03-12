28ddfc955972dca6778fcf75894f615ad3142542
Kevin giordani <giordani.kevin.02@gmail.com>
2018-03-12 17:36:51 +0100

    Enelevé le focus sur le txfd pseudo

--- client/LoginScene.java
+++ client/LoginScene.java
@@ -37,6 +37,7 @@ public class LoginScene extends MyScene{
 	public LoginScene(Group root,double width, double height) {
 		super(root, width,height, ClientFX.BACKGROUND_COLOR);
         root.requestFocus();
+        
         System.out.println("Width : " + this.width + " Height : " + this.height);
                 
         pseudoTextField = new MyTextFieldFX("Pseudo", pseudoBounds, this.width, this.height);

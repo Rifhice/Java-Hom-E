359bc31b020c9f2ea83de8bc19c7e0ab6024af17
Clément Roig <clm.roig@gmail.com>
2018-03-15 19:10:14 +0100

    [Actuator model] Add external attributes

--- Server/models/Actuator.java
+++ Server/models/Actuator.java
@@ -14,6 +14,11 @@ public class Actuator extends ExternalActor{
     // ==================== //
 	private ArrayList<Command> commands;
 	
+	// Attributes from others tables
+	private int category_actuator_id;
+	private String category_actuator_name;
+	private String category_actuator_description;
+	
 	// ====================== //
     // ==== CONSTRUCTORS ==== //
     // ====================== //
@@ -25,6 +30,14 @@ public class Actuator extends ExternalActor{
 		super(name,description);
 		this.commands = commands;
 	}
+	
+	public Actuator(String name, String description,ArrayList<Command> commands, int category_actuator_id, String category_actuator_name, String category_actuator_description) {
+        super(name,description);
+        this.commands = commands;
+        this.category_actuator_id = category_actuator_id;
+        this.category_actuator_name = category_actuator_name;
+        this.category_actuator_description = category_actuator_description;
+    }
 	
 	// ================= //
     // ==== METHODS ==== //

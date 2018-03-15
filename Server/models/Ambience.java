f8966a9dcb97ac7ece3cf82df4314387928f3790
Clément Roig <clm.roig@gmail.com>
2018-03-15 19:51:22 +0100

    [Ambience Model] Get/set + indent

--- Server/models/Ambience.java
+++ Server/models/Ambience.java
@@ -8,49 +8,55 @@ public class Ambience {
     // ==================== //
     // ==== ATTRIBUTES ==== //
     // ==================== //
-	private ArrayList<Behaviour> behaviours;
-	private String name;
-	
+    private ArrayList<Behaviour> behaviours;
+    private String name;
+
     // ====================== //
     // ==== CONSTRUCTORS ==== //
     // ====================== //
-	public Ambience(String name, ArrayList<Behaviour> behaviours) {
-		this.name = name;
-		this.behaviours = behaviours;
-	}
-	
+    public Ambience() {}
+
+    public Ambience(String name, ArrayList<Behaviour> behaviours) {
+        this.name = name;
+        this.behaviours = behaviours;
+    }
+
     // ================= //
     // ==== METHODS ==== //
-    // ================= //	
-	public void activateAmbience() {
-		for (int i = 0; i < behaviours.size(); i++) {
-			behaviours.get(i).activate();
-		}
-	}
-	
-	public void deactivateAmbience() {
-		for (int i = 0; i < behaviours.size(); i++) {
-			behaviours.get(i).deactivate();
-		}
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void addBehaviour(Behaviour behaviour) {
-		behaviours.add(behaviour);
-	}
-	
-	public void deleteBehaviour(Behaviour behaviour) {
-		behaviours.remove(behaviour);
-	}
-	
-	public ArrayList<Behaviour> getBehaviours(){
-		return behaviours;
-	}
+    // ================= //		
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public void setBehaviours(ArrayList<Behaviour> behaviours) {
+        this.behaviours = behaviours;
+    }
+
+    public ArrayList<Behaviour> getBehaviours(){
+        return behaviours;
+    }
+    
+    public void addBehaviour(Behaviour behaviour) {
+        behaviours.add(behaviour);
+    }
+
+    public void deleteBehaviour(Behaviour behaviour) {
+        behaviours.remove(behaviour);
+    }
+
+    public void activateAmbience() {
+        for (int i = 0; i < behaviours.size(); i++) {
+            behaviours.get(i).activate();
+        }
+    }
+
+    public void deactivateAmbience() {
+        for (int i = 0; i < behaviours.size(); i++) {
+            behaviours.get(i).deactivate();
+        }
+    }
 }

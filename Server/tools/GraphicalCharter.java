d765d5174a58a4419698b4dc57c0a8fb41f9dd10
Clément Roig <clm.roig@gmail.com>
2018-03-15 22:48:33 +0100

    [Client/Tool] Graphical Charter

---
+++ Server/tools/GraphicalCharter.java
@@ -0,0 +1,19 @@
+package tools;
+
+import javafx.scene.paint.Color;
+/**
+ * Contains all the colors / elements of the graphical charter of Hom-E
+ * @author Clm-Roig
+ */
+public class GraphicalCharter {
+    /**
+     * Hom-E Colors
+     */
+    public static Color RED = Color.web("#71F481");
+    public static Color LIGHT_GREEN = Color.web("#71F481");
+    public static Color DARK_GREEN = Color.web("#2FA83E");
+    public static Color LIGHT_BLUE = Color.web("#5EADF6");
+    public static Color DEEP_BLUE = Color.web("#0F204F");
+    public static Color LIGHT_GRAY = Color.web("#C6CDE0");
+    
+}

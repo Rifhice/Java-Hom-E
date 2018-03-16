992660008507654f23f72f2a8183a559a1df27af
Unknown <jhennebert@hotmail.fr>
2018-03-16 17:35:03 +0100

    [Model DiscreteValue]

--- src/server/models/DiscreteValue.java
+++ src/server/models/DiscreteValue.java
@@ -1,5 +1,43 @@
 package server.models;
 
-public class DiscreteValue {
+import java.util.ArrayList;
+
+/**
+ * A discrete environment variable is a variable which the current value is a String 
+ * among possible values.
+ * @author Clm-Roig
+ */
+public class DiscreteValue extends Value {
+
+    // ==================== //
+    // ==== ATTRIBUTES ==== //
+    // ==================== //
+    private ArrayList<String> possibleValues;
+
+    // ====================== //
+    // ==== CONSTRUCTORS ==== //
+    // ====================== //
+    
+    public DiscreteValue() {}
+    
+    public DiscreteValue(String name, ArrayList<String> values) {
+        super(name);
+        this.possibleValues = values;   
+    }
+
+    // ================= //
+    // ==== METHODS ==== //
+    // ================= //	    
+    public ArrayList<String> getPossibleValues() {
+        return possibleValues;
+    }
+
+    public void setPossibleValues(ArrayList<String> values) {
+        this.possibleValues = values;
+    }
+ 
+    
+    // ==================================
+
 
 }

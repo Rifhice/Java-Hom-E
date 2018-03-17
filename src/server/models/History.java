d7f04ca5c266c68604426e9e744ea88ed8059b40
Clément Roig <clm.roig@gmail.com>
2018-03-17 17:57:19 +0100

    [History Model] Done

---
+++ src/server/models/History.java
@@ -0,0 +1,78 @@
+package server.models;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Clm-Roig
+ */
+public abstract class History {
+	    
+	// ==================== //
+	// ==== ATTRIBUTES ==== //
+	// ==================== //
+	private int id;
+	private Date date;
+	private String type;
+	private String action;
+	private String user;
+
+	// ====================== //
+	// ==== CONSTRUCTORS ==== //
+	// ====================== //
+	public History() {}
+	    
+	public History(Date date, String type, String action, String user) {
+		this.date = date;
+		this.type = type;
+		this.action = action;
+		this.user = user;
+	}
+	
+	// ================= //
+    // ==== METHODS ==== //
+    // ================= //
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+    
+    // ===========================================
+		
+
+}

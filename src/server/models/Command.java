220e76005d88ab4833b96b3559eeade0233a6090
Clément Roig <clm.roig@gmail.com>
2018-03-17 17:43:42 +0100

    [Command Model] done

--- src/server/models/Command.java
+++ src/server/models/Command.java
@@ -4,8 +4,59 @@ package server.models;
  * A command is the information about the possible values (continuous or discrete) an actuator can receive to perform one action
  * @author Clm-Roig
  */
-public abstract class Command {
+public class Command {
 	
-	public abstract void launch();
+    // ==================== //
+    // ==== ATTRIBUTES ==== //
+    // ==================== //
+    private int id;
+    private String name;
+    
+    // Attributes from other tables
+    private int actuatorId;
+    
+    // ====================== //
+    // ==== CONSTRUCTORS ==== //
+    // ====================== //
+    public Command() {}
+    
+    public Command(String name) {
+        this.name = name;
+    }
+    
+    public Command(String name, int actuatorId) {
+        this.name = name;
+        this.actuatorId = actuatorId;
+    }
+
+    // ================= //
+    // ==== METHODS ==== //
+    // ================= //
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getActuatorId() {
+        return actuatorId;
+    }
+
+    public void setActuatorId(int actuatorId) {
+        this.actuatorId = actuatorId;
+    }
+    
+    // =================================================    
+	
 	
 }

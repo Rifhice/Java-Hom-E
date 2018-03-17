c84b9b30ea6f79dd00413cefaec0ae5511ccb4e5
Clément Roig <clm.roig@gmail.com>
2018-03-17 17:44:27 +0100

    [Value Model] Add get/set

--- src/server/models/Value.java
+++ src/server/models/Value.java
@@ -24,6 +24,28 @@ public abstract class Value {
 	public Value(String name) {
 		this.name = name;
 	}
+	
+	// ================= //
+    // ==== METHODS ==== //
+    // ================= //
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    // ===========================================
+	
 		
 
 }

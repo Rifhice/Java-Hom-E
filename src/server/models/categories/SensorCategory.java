78f7a23d2233b18f428e1ffd90dfd0c914eaec5a
Clément Roig <clm.roig@gmail.com>
2018-03-19 16:27:54 +0100

    [Sensor Category model] done

--- src/server/models/categories/SensorCategory.java
+++ src/server/models/categories/SensorCategory.java
@@ -1,17 +1,44 @@
 package server.models.categories;
 
-public class SensorCategory extends Category{
+import java.util.ArrayList;
+import server.models.Sensor;
 
-	public SensorCategory() {}
-	
-	public SensorCategory(int id, String name, String description) {
-		super(id, name, description);
-	}
-	public SensorCategory(String name, String description) {
-		super(name, description);
-	}
-	
-	public String toString() {
-		return "Sensor category : " + id + "\nName : " + name + "\nDescription : " + description;
-	}
+public class SensorCategory extends Category{
+    // ==================== //
+    // ==== ATTRIBUTES ==== //
+    // ==================== //
+    ArrayList<Sensor> sensors;
+
+    // ====================== //
+    // ==== CONSTRUCTORS ==== //
+    // ====================== //
+    public SensorCategory() {}
+
+    public SensorCategory(String name, String description) {
+        super(name, description);
+    }
+
+    public SensorCategory(int id, String name, String description) {
+        super(id, name, description);
+    }
+    
+    public SensorCategory(int id, String name, String description, ArrayList<Sensor> sensors) {
+        super(id, name, description);
+        this.sensors = sensors;
+    }
+
+    // ================= //
+    // ==== METHODS ==== //
+    // ================= //
+    public ArrayList<Sensor> getSensors() {
+        return sensors;
+    }
+
+    public void setSensors(ArrayList<Sensor> sensors) {
+        this.sensors = sensors;
+    }
+    
+    public String toString() {
+        return "Sensor category : " + id + "\nName : " + name + "\nDescription : " + description;
+    }
 }

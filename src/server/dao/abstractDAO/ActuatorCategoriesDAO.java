d1a32e973905c330a2f19ff8295ac30d4ec08582
Clément Roig <clm.roig@gmail.com>
2018-03-19 08:06:34 +0100

    [DAO] Abstracting DAO

--- src/server/dao/abstractDAO/ActuatorCategoriesDAO.java
+++ src/server/dao/abstractDAO/ActuatorCategoriesDAO.java
@@ -1,44 +1,20 @@
 package server.dao.abstractDAO;
 
 import java.sql.Connection;
-import java.util.ArrayList;
 
 import server.models.categories.ActuatorCategory;
 
-public class ActuatorCategoriesDAO extends DAO<ActuatorCategory>{
+public abstract class ActuatorCategoriesDAO extends DAO<ActuatorCategory>{
 
-	public ActuatorCategoriesDAO(Connection connect) {
-		super(connect);
+    // ====================== //
+    // ==== CONSTRUCTORS ==== //
+    // ====================== //
+    public ActuatorCategoriesDAO(Connection connect) {
+        super(connect);
 	}
-
-	@Override
-	public ActuatorCategory create(ActuatorCategory obj) throws DAOException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ActuatorCategory getById(int id) throws DAOException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean update(ActuatorCategory obj) throws DAOException {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public int delete(int id) throws DAOException {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public ArrayList<ActuatorCategory> getAll() throws DAOException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    
+    // ================= //
+    // ==== METHODS ==== //
+    // ================= //
 
 }

dbb80d522c1091a6f968ec5a67c0018ee1725715
Clément Roig <clm.roig@gmail.com>
2018-03-20 22:26:40 +0100

    [MyTextArea] Fix blurry TextArea
    Don't work everytime though...

--- src/user/ui/componentJavaFX/MyTextArea.java
+++ src/user/ui/componentJavaFX/MyTextArea.java
@@ -1,5 +1,7 @@
 package user.ui.componentJavaFX;
 import java.awt.geom.Rectangle2D;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 import javafx.scene.control.TextArea;
 import javafx.scene.text.Font;
@@ -7,22 +9,42 @@ import user.tools.GraphicalCharter;
 
 public class MyTextArea extends TextArea{
 	
-	public MyTextArea(String text,Rectangle2D.Float bounds) {
-		setText(text);
-		setLayoutX(bounds.getX());
-		setLayoutY(bounds.getY());
-		setPrefWidth(bounds.getWidth());
-		setPrefHeight(bounds.getHeight());
-		setFont(Font.font(GraphicalCharter.FONT,GraphicalCharter.FONT_SIZE));
+	public MyTextArea(String text,Rectangle2D.Float bounds) {	
+		// Round the width & height to resolve "blurry" textArea bug
+		DecimalFormat df = new DecimalFormat("#");
+        df.setMaximumFractionDigits(0);
+        
+		int widthRounded = Integer.parseInt(df.format(bounds.getWidth()));
+	    int heightRounded = Integer.parseInt(df.format(bounds.getHeight()));
+	    int xRounded = Integer.parseInt(df.format(bounds.getX()));
+	    int yRounded = Integer.parseInt(df.format(bounds.getY()));
+
+	    setPrefWidth(widthRounded);
+        setPrefHeight(heightRounded);
+        setLayoutX(xRounded);
+        setLayoutY(yRounded);
+        
+        setText(text);
+        setFont(Font.font(GraphicalCharter.FONT,GraphicalCharter.FONT_SIZE));
 	}
 	
-	public MyTextArea(String text,Rectangle2D.Float bounds,float fontMultiplier) {
-		setText(text);
-		setLayoutX(bounds.getX());
-		setLayoutY(bounds.getY());
-		setPrefWidth(bounds.getWidth());
-		setPrefHeight(bounds.getHeight());
-		setFont(Font.font(GraphicalCharter.FONT,GraphicalCharter.FONT_SIZE * fontMultiplier));
+	public MyTextArea(String text,Rectangle2D.Float bounds,float fontMultiplier) {	
+		// Round the width & height to resolve "blurry" textArea bug
+        DecimalFormat df = new DecimalFormat("#");
+        df.setMaximumFractionDigits(0);
+        
+        int widthRounded = Integer.parseInt(df.format(bounds.getWidth()));
+        int heightRounded = Integer.parseInt(df.format(bounds.getHeight()));
+        int xRounded = Integer.parseInt(df.format(bounds.getX()));
+        int yRounded = Integer.parseInt(df.format(bounds.getY()));
+        
+        setPrefWidth(widthRounded);
+        setPrefHeight(heightRounded);
+        setLayoutX(xRounded);
+        setLayoutY(yRounded);
+        
+        setText(text);
+        setFont(Font.font(GraphicalCharter.FONT,GraphicalCharter.FONT_SIZE * fontMultiplier));
 	}
 	
 	public MyTextArea lockText() {

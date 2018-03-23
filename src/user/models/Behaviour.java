f9b22d3193a533c4dc94672cbc7fec5e2dd40315
Clément Roig <clm.roig@gmail.com>
2018-03-23 16:01:34 +0100

    [Model Client Behaviour] add isActivated

--- src/user/models/Behaviour.java
+++ src/user/models/Behaviour.java
@@ -3,17 +3,30 @@ package user.models;
 import server.models.evaluable.Expression;
 
 public class Behaviour {
-	 // ==================== //
+	// ==================== //
     // ==== ATTRIBUTES ==== //
     // ==================== //
     private int id;
 	private String name;
+	private boolean isActivated;
 	
+	// ====================== //
+    // ==== CONSTRUCTORS ==== //
+    // ====================== //
 	public Behaviour(int id, String name) {
 		this.id = id;
 		this.name = name;
 	}
 	
+	public Behaviour(int id, String name, Boolean isActivated) {
+        this.id = id;
+        this.name = name;
+        this.isActivated = isActivated;
+    }
+	
+	// ================= //
+    // ==== METHODS ==== //
+    // ================= //	
 	public int getId() {
 		return id;
 	}
@@ -26,5 +39,13 @@ public class Behaviour {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+    public boolean isActivated() {
+        return isActivated;
+    }
+
+    public void setActivated(boolean isActivated) {
+        this.isActivated = isActivated;
+    }
 	
 }

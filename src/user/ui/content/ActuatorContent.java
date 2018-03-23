577fb85fce4d3c99f0b84157049dc5f622241fe7
Clément Roig <clm.roig@gmail.com>
2018-03-23 14:47:43 +0100

    [ActuatorContent] The beginning

--- src/user/ui/content/ActuatorContent.java
+++ src/user/ui/content/ActuatorContent.java
@@ -1,13 +1,32 @@
 package user.ui.content;
 
 import javafx.scene.control.Label;
+import user.ui.componentJavaFX.MyPane;
+import user.ui.componentJavaFX.MyRectangle;
 
 public class ActuatorContent extends Content {
 
 	private static ActuatorContent content = null;
+	
+	private MyRectangle actuatorsListBounds = new MyRectangle(0f, 0f, 0.2f, 1.0f);
+	private MyRectangle currentActuatorBounds = new MyRectangle(0.2f, 0f, 0.4f, 1.0f);
+    private MyRectangle latestActionsBounds = new MyRectangle(0.6f, 0f, 0.4f, 1.0f);
+
 
 	private ActuatorContent() {
-		this.getChildren().add(new Label("Actuator"));
+	    MyPane actuatorsListPane = new MyPane(actuatorsListBounds.computeBounds(width, height));
+	    actuatorsListPane.setStyle("-fx-background-color: rgb(255,0,0)");
+	    MyPane currentActuatorPane = new MyPane(currentActuatorBounds.computeBounds(width, height));
+	    currentActuatorPane.setStyle("-fx-background-color: rgb(0,0,255)");
+	    MyPane latestActionsPane = new MyPane(latestActionsBounds.computeBounds(width, height));
+	    latestActionsPane.setStyle("-fx-background-color: rgb(0,255,0)");
+
+	    this.getChildren().add(actuatorsListPane);
+	    this.getChildren().add(currentActuatorPane);
+	    this.getChildren().add(latestActionsPane);
+	    actuatorsListPane.getChildren().add(new Label("Actuators List"));
+	    currentActuatorPane.getChildren().add(new Label("Actuator selected"));
+	    latestActionsPane.getChildren().add(new Label("Latest Actions"));
 	}
 
 	public static Content getInstance() {

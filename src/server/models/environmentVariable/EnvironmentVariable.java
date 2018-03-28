bad093ab4282ad7ab43d5244334cd12837762977
Clément Roig <clm.roig@gmail.com>
2018-03-28 16:04:01 +0200

    [SensorManager] getAllEv() returns values

--- src/server/models/environmentVariable/EnvironmentVariable.java
+++ src/server/models/environmentVariable/EnvironmentVariable.java
@@ -132,6 +132,7 @@ public class EnvironmentVariable extends Observable{
     	result.put("name", name);
     	result.put("unit", unit);
     	result.put("description", description);
+    	result.put("value", value);
     	return result;
     }
 
